1dbff76d2c20f681ba7e6a850e3f1eecc1116172
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-03-12 18:52:15 +0900

    Feat run osc

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using Rug.Osc;
+
+IPAddress address = IPAddress.Parse("127.0.0.1");
+int sendPort = 9000;
+var oscSender = new OscSender(address, sendPort);
+oscSender.Send(new OscMessage("/input/Jump", 1));
+oscSender.Connect();
+oscSender.Close();
+
 Console.WriteLine("Hello, World!");
